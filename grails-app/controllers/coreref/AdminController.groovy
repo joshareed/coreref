3ea78f8c1041c63ce5ad11ae9fa2b291454991d2
Josh Reed <jareed@andrill.org>
2009-12-09 20:27:35 -0600

    Converted spaces to tabs and removed trailing spaces
    --HG--
    extra : convert_revision : 54472e62e143419e7bd65ea16c27a5a29681f2d0

--- grails-app/controllers/coreref/AdminController.groovy
+++ grails-app/controllers/coreref/AdminController.groovy
@@ -1,24 +1,24 @@
 package coreref
 
 class AdminController {
-    def mongoService
+	def mongoService
 
-    def updateIndex = {
-        long start = System.currentTimeMillis()
-        def collection = mongoService.getCollection(params.collection)
-        if (collection) {
-            collection.find().each { doc ->
-                def tokens = []
-                ['name', 'code', 'text'].each { field ->
-                    if (doc.containsKey(field)) {
-                        SearchUtils.tokenize(doc[field] as String, tokens)
-                    }
-                }
-                if (tokens) {
-                    collection.update(doc, ['$set': ['_keywords': tokens]] as com.mongodb.BasicDBObject)
-                }
-            }
-        }
-        render "Text indexing of ${params.collection} took ${System.currentTimeMillis() - start} ms"
-    }
+	def updateIndex = {
+		long start = System.currentTimeMillis()
+		def collection = mongoService.getCollection(params.collection)
+		if (collection) {
+			collection.find().each { doc ->
+				def tokens = []
+				['name', 'code', 'text'].each { field ->
+					if (doc.containsKey(field)) {
+						SearchUtils.tokenize(doc[field] as String, tokens)
+					}
+				}
+				if (tokens) {
+					collection.update(doc, ['$set': ['_keywords': tokens]] as com.mongodb.BasicDBObject)
+				}
+			}
+		}
+		render "Text indexing of ${params.collection} took ${System.currentTimeMillis() - start} ms"
+	}
 }
