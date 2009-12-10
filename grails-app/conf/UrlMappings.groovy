ac7c68580d5ddd5eb97f0993453dde9c99fc256c
Josh Reed <jareed@andrill.org>
2009-12-09 20:30:00 -0600

    Added a URL mapping for the search controller
    --HG--
    extra : convert_revision : d67dad46df1329215b94c5bffec2966ba17e8e01

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -2,6 +2,7 @@ class UrlMappings {
 	static mappings = {
 		"/admin/$action/$collection/$opt?"(controller: 'admin')
 		"/services/track/$action/$collection/$top/$base?/$scale?"(controller: 'track')
+		"/services/search/$action/$collection/$query/$top?/$base?"(controller: 'search')
 		"/"(view:"/index")
 		"500"(view:'/error')
 	}
