96bfaadd2b7a0b3c613348f104df9a274f2b42b1
Josh Reed <jareed@andrill.org>
2009-12-10 20:54:12 -0600

    Updated the query handling and the error reporting
    --HG--
    extra : convert_revision : 764f88a06655176a7a8ec405f852c9e27e855651

--- grails-app/controllers/coreref/SearchController.groovy
+++ grails-app/controllers/coreref/SearchController.groovy
@@ -18,23 +18,28 @@ class SearchController {
 	def type = {
 		def collection = mongoService.getCollection(params.collection)
 		if (collection) {
-			def query = QueryUtils.withDepths(params, ['class': params.query])
+			def query = QueryUtils.withDepths(params, ['class': (params.query ?: '')])
 			def results = collection.find(query, QueryUtils.buildFilter(params)).collect() { SearchUtils.clean(it) }
 			render "${results as JSON}"
+		} else {
+			response.sendError(404, "Invalid collection: '${params.collection}'")
 		}
 	}
 
 	def text = {
 		def collection = mongoService.getCollection(params.collection)
 		if (collection) {
+			def q = params.query ?: ''
 			def query = QueryUtils.withDepths(params)
-			if (params.query.contains('|')) {
-				query['_keywords'] = ['$in': SearchUtils.tokenize(params.query, [])]
+			if (q.contains('|')) {
+				query['_keywords'] = ['$in': SearchUtils.tokenize(q, [])]
 			} else {
-				query['_keywords'] = ['$all': SearchUtils.tokenize(params.query, [])]
+				query['_keywords'] = ['$all': SearchUtils.tokenize(q, [])]
 			}
 			def results = collection.find(query, QueryUtils.buildFilter(params)).collect() { SearchUtils.clean(it) }
 			render "${results as JSON}"
+		} else {
+			response.sendError(404, "Invalid collection: '${params.collection}'")
 		}
 	}
 
@@ -43,9 +48,11 @@ class SearchController {
 		if (collection) {
 			def filter = [top: true, base: true]
 			def query = QueryUtils.withDepths(params, ['class': 'Datum'])
-			params.query.split(',').each { query[it] = ['$exists': true]; filter[it] = true }
+			(params.query ?: '').split(',').each { query[it] = ['$exists': true]; filter[it] = true }
 			def results = collection.find(query, filter).collect() { SearchUtils.clean(it) }
 			render "${results as JSON}"
+		} else {
+			response.sendError(404, "Invalid collection: '${params.collection}'")
 		}
 	}
 }
