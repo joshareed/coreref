9718fecf35a51e3b455a8ce89a379736c7db7ef8
Josh Reed <jareed@andrill.org>
2009-12-10 20:50:10 -0600

    Updated the search URL mapping to make the query optional
    --HG--
    extra : convert_revision : 82ddd6d270fc2bd84489d19a9a4953f4bbfafce3

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -2,7 +2,7 @@ class UrlMappings {
 	static mappings = {
 		"/admin/$action/$collection/$opt?"(controller: 'admin')
 		"/services/track/$action/$collection/$top/$base?/$scale?"(controller: 'track')
-		"/services/search/$action/$collection/$query/$top?/$base?"(controller: 'search')
+		"/services/search/$action/$collection/$query?/$top?/$base?"(controller: 'search')
 		"/"(view:"/index")
 		"500"(view:'/error')
 	}
