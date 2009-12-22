21f201f722b430c9985307984fcd45923667a726
Josh Reed <jareed@andrill.org>
2009-12-22 14:03:49 -0600

    Added 'download' and 'timeline' actions
    --HG--
    extra : convert_revision : e115dfee6d21c5f112d5f501242982cd2b635de6

--- grails-app/controllers/coreref/ProjectController.groovy
+++ grails-app/controllers/coreref/ProjectController.groovy
@@ -4,9 +4,9 @@ import grails.converters.JSON
 
 class ProjectController {
 	def mongoService
-	
+
 	def index = { redirect(action: overview, params: params) }
-	
+
 	def overview = {
 		def collection = mongoService.getCollection('projects')
 		if (collection) {
@@ -20,8 +20,36 @@ class ProjectController {
 			sendError(500, "'projects' collection not defined")
 		}
 	}
-	
+
 	def viewer = {
+		def collection = mongoService.getCollection('projects')
+		if (collection) {
+			def project = collection.find(collection: params.collection).find { true }
+			if (project) {
+				return [project: project, depth: params.depth ?: 0]
+			} else {
+				sendError(404, "Invalid collection: '${params.collection}'")
+			}
+		} else {
+			sendError(500, "'projects' collection not defined")
+		}
+	}
+
+	def downloads = {
+		def collection = mongoService.getCollection('projects')
+		if (collection) {
+			def project = collection.find(collection: params.collection).find { true }
+			if (project) {
+				return [project: project, depth: params.depth ?: 0]
+			} else {
+				sendError(404, "Invalid collection: '${params.collection}'")
+			}
+		} else {
+			sendError(500, "'projects' collection not defined")
+		}
+	}
+
+	def timeline = {
 		def collection = mongoService.getCollection('projects')
 		if (collection) {
 			def project = collection.find(collection: params.collection).find { true }
