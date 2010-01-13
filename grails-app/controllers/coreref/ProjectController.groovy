5b04c05369f3c2b733fd7fa90ef99bdac510d06d
Josh Reed <jareed@andrill.org>
2010-01-13 12:49:21 -0600

    Updated to use '_projects' instead of 'projects' for the metadata collection
    --HG--
    extra : convert_revision : 25ccf767a256f5ce455f20a8ab0e46f8f2439d29

--- grails-app/controllers/coreref/ProjectController.groovy
+++ grails-app/controllers/coreref/ProjectController.groovy
@@ -1,65 +1,63 @@
 package coreref
 
-import grails.converters.JSON
-
 class ProjectController {
 	def mongoService
 
 	def index = { redirect(action: overview, params: params) }
 
 	def overview = {
-		def collection = mongoService.getCollection('projects')
+		def collection = mongoService.getCollection('_projects')
 		if (collection) {
-			def project = collection.find(collection: params.collection).find { true }
+			def project = collection.find(project: params.collection).find { true }
 			if (project) {
 				return [project: project]
 			} else {
-				sendError(404, "Invalid collection: '${params.collection}'")
+				sendError(404, "Invalid project: '${params.collection}'")
 			}
 		} else {
-			sendError(500, "'projects' collection not defined")
+			sendError(500, "'_projects' collection not defined")
 		}
 	}
 
 	def viewer = {
-		def collection = mongoService.getCollection('projects')
+		def collection = mongoService.getCollection('_projects')
 		if (collection) {
-			def project = collection.find(collection: params.collection).find { true }
+			def project = collection.find(project: params.collection).find { true }
 			if (project) {
 				return [project: project, depth: params.depth ?: 0]
 			} else {
-				sendError(404, "Invalid collection: '${params.collection}'")
+				sendError(404, "Invalid project: '${params.collection}'")
 			}
 		} else {
-			sendError(500, "'projects' collection not defined")
+			sendError(500, "'_projects' collection not defined")
 		}
 	}
 
 	def downloads = {
-		def collection = mongoService.getCollection('projects')
+		def collection = mongoService.getCollection('_projects')
 		if (collection) {
-			def project = collection.find(collection: params.collection).find { true }
+			def project = collection.find(project: params.collection).find { true }
 			if (project) {
 				return [project: project, depth: params.depth ?: 0]
 			} else {
-				sendError(404, "Invalid collection: '${params.collection}'")
+				sendError(404, "Invalid project: '${params.collection}'")
 			}
 		} else {
-			sendError(500, "'projects' collection not defined")
+			sendError(500, "'_projects' collection not defined")
 		}
 	}
 
 	def timeline = {
-		def collection = mongoService.getCollection('projects')
+		def collection = mongoService.getCollection('_projects')
 		if (collection) {
-			def project = collection.find(collection: params.collection).find { true }
+			def project = collection.find(project: params.collection).find { true }
 			if (project) {
 				return [project: project, depth: params.depth ?: 0]
 			} else {
-				sendError(404, "Invalid collection: '${params.collection}'")
+				sendError(404, "Invalid project: '${params.collection}'")
 			}
 		} else {
-			sendError(500, "'projects' collection not defined")
+			sendError(500, "'_projects' collection not defined")
 		}
 	}
 }
