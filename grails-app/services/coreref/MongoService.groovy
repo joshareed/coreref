5111bacfaca673bf6ec40ab71c4c88b6e1e36072
Josh Reed <jareed@andrill.org>
2010-01-25 15:27:07 -0600

    Made MongoService a bit more GORMish by adding property-like access for collections and supporting find, findAll, findBy, and findAllBy methods.
    --HG--
    extra : convert_revision : 1513de0d07a9071e8dce0e3fc3594f03c8b5e4af

--- grails-app/services/coreref/MongoService.groovy
+++ grails-app/services/coreref/MongoService.groovy
@@ -6,17 +6,47 @@ import com.mongodb.*
 class MongoService {
 	private Mongo mongo
 	boolean transactional = false
+
 	static {
 		// some convenience methods
 		DBCollection.metaClass {
-			find << { LinkedHashMap query -> delegate.find(query as BasicDBObject) }
-			find << { LinkedHashMap query, LinkedHashMap filter -> delegate.find(query as BasicDBObject, filter as BasicDBObject) }
+			findAll << { LinkedHashMap query -> delegate.find(query as BasicDBObject) }
+			findAll << { LinkedHashMap query, LinkedHashMap filter -> delegate.find(query as BasicDBObject, filter as BasicDBObject) }
+			find << { LinkedHashMap query -> delegate.find(query as BasicDBObject).find {true} }
+			find << { LinkedHashMap query, LinkedHashMap filter -> delegate.find(query as BasicDBObject, filter as BasicDBObject).find {true} }
+			methodMissing { String name, args ->
+				if (name.startsWith('findBy')) {
+					def p =	 name - 'findBy'
+					p = p[0].toLowerCase() + p[1..-1]
+					def query = [:]
+					query[p] = args[0]
+					return delegate.find(query as BasicDBObject).find { true }
+				} else if (name.startsWith('findAllBy')) {
+					def p =	 name - 'findAllBy'
+					p = p[0].toLowerCase() + p[1..-1]
+					def query = [:]
+					query[p] = args[0]
+					return delegate.find(query as BasicDBObject)
+				}
+				throw new MissingMethodException(name, delegate, args)
+			}
 		}
 		DBCursor.metaClass {
 			sort << { LinkedHashMap keys -> delegate.sort(keys as BasicDBObject) }
+			first << { -> delegate.find { true } }
 		}
 	}
 
+	// allow accessing collections like properties.
+	def propertyMissing(name) {
+		def collection = getCollection(name)
+		if (collection) {
+			MongoService.metaClass."$name" = collection
+		}
+		return collection
+	}
+
+	// get a collection by name.  If the collection doesn't exist, this method return null.
 	def getCollection(name) {
 		if (!name) { return null }
 		if (!mongo) {
