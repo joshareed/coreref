351bcea53f56f574aa2eadc1c0d69487341ea852
Josh Reed <jareed@andrill.org>
2010-01-25 16:22:17 -0600

    Updated to be more DRY
    --HG--
    extra : convert_revision : b6aca0a0f5c13cb742f23cab90a74f0360847e4b

--- grails-app/controllers/coreref/SearchController.groovy
+++ grails-app/controllers/coreref/SearchController.groovy
@@ -6,61 +6,45 @@ class SearchController {
 	def mongoService
 
 	def type = {
-		def collection = mongoService.getCollection(params.collection)
-		if (collection) {
-			def query = QueryUtils.withDepths(params, ['class': (params.query ?: '')])
-			def results = collection.find(query, QueryUtils.buildFilter(params)).sort([top: 1]).collect() { SearchUtils.clean(it) }
-			if (params.callback) {
-				render(contentType: 'application/json', text: "${params.callback}(${results as JSON})")
-			} else {
-				render(contentType: 'application/json', text: (results as JSON))
-			}
-		} else {
-			response.sendError(404, "Invalid collection: '${params.collection}'")
-		}
+		renderQuery(['class': (params.query ?: '')])
 	}
 
 	def text = {
-		def collection = mongoService.getCollection(params.collection)
-		if (collection) {
-			def q = params.query ?: ''
-			def query = QueryUtils.withDepths(params)
-			if (q.contains('|')) {
-				query['_keywords'] = ['$in': SearchUtils.tokenize(q, [])]
-			} else {
-				query['_keywords'] = ['$all': SearchUtils.tokenize(q, [])]
-			}
-			def results = collection.find(query, QueryUtils.buildFilter(params)).sort([top: 1]).collect() { SearchUtils.clean(it) }
-			if (params.callback) {
-				render(contentType: 'application/json', text: "${params.callback}(${results as JSON})")
-			} else {
-				render(contentType: 'application/json', text: (results as JSON))
-			}
+		def query = [:]
+		def q = params.query ?: ''
+		if (q.contains('|')) {
+			query['_keywords'] = ['$in': SearchUtils.tokenize(q, [])]
 		} else {
-			response.sendError(404, "Invalid collection: '${params.collection}'")
+			query['_keywords'] = ['$all': SearchUtils.tokenize(q, [])]
 		}
+		renderQuery(query)
 	}
 
 	def data = {
-		def collection = mongoService.getCollection(params.collection)
-		if (collection) {
-			def filter = [top: true, base: true]
-			def query = QueryUtils.withDepths(params, ['class': 'Datum'])
-			(params.query ?: '').split(',').each { query[it] = ['$exists': true]; filter[it] = true }
-			def results = [:]
-			collection.find(query, filter).sort([top: 1]).collect() { SearchUtils.clean(it) }.each { doc ->
-				doc.keySet().findAll{ it != 'top' && it != 'base' && it[0] != '_' }.each { k ->
-					if (!results[k]) results[k] = [label: k[0].toUpperCase() + k[1..-1], data: []]
-					results[k].data << [doc.top, doc[k]]
-				}
+		def filter = [top: true, base: true]
+		def query = ['class': 'Datum']
+		(params.query ?: '').split(',').each { query[it] = ['$exists': true]; filter[it] = true }
+		def results = [:]
+		mongoService[params.project].find(query, filter).sort([top: 1]).collect() { SearchUtils.clean(it) }.each { doc ->
+			doc.keySet().findAll{ it != 'top' && it != 'base' && it[0] != '_' }.each { k ->
+				if (!results[k]) results[k] = [label: k[0].toUpperCase() + k[1..-1], data: []]
+				results[k].data << [doc.top, doc[k]]
 			}
-			if (params.callback) {
-				render(contentType: 'application/json', text: "${params.callback}(${results as JSON})")
-			} else {
-				render(contentType: 'application/json', text: (results as JSON))
-			}
-		} else {
-			response.sendError(404, "Invalid collection: '${params.collection}'")
 		}
+		renderResults(results)
+	}
+
+	def renderResults = { results ->
+		if (params.callback) {
+			render(contentType: 'application/json', text: "${params.callback}(${results as JSON})")
+		} else {
+			render(contentType: 'application/json', text: (results as JSON))
+		}
+	}
+
+	def renderQuery = { query, filter = [:], sort = [top: 1] ->
+		renderResults(mongoService[params.project].findAll(
+			QueryUtils.withDepths(params, query),
+			QueryUtils.buildFilter(params, filter)).sort(sort).collect() { SearchUtils.clean(it) })
 	}
 }
