b43cb500b965dbf2b2d50788b6f31d53102b9044
Josh Reed <jareed@andrill.org>
2010-01-26 10:58:33 -0600

    Updated to use the new MongoService interaction style
    --HG--
    extra : convert_revision : 387a239631469c4830cb8d5a6739267d69ba6ea1

--- grails-app/controllers/coreref/AdminController.groovy
+++ grails-app/controllers/coreref/AdminController.groovy
@@ -4,23 +4,19 @@ class AdminController {
 	def mongoService
 
 	def updateIndex = {
-		def collection = mongoService.getCollection(params.collection)
-		if (collection) {
 		long start = System.currentTimeMillis()
-			collection.find().each { doc ->
-				def tokens = []
-				['name', 'code', 'text'].each { field ->
-					if (doc.containsKey(field)) {
-						SearchUtils.tokenize(doc[field] as String, tokens)
-					}
-				}
-				if (tokens) {
-					collection.update(doc, ['$set': ['_keywords': tokens]] as com.mongodb.BasicDBObject)
+		def collection = mongoService[params.project]
+		collection.find().each { doc ->
+			def tokens = []
+			['name', 'code', 'text'].each { field ->
+				if (doc.containsKey(field)) {
+					SearchUtils.tokenize(doc[field] as String, tokens)
 				}
 			}
-			render "Text indexing of ${params.collection} took ${System.currentTimeMillis() - start} ms"
-		} else {
-			response.sendError(404, "Invalid collection: '${params.collection}'")
+			if (tokens) {
+				collection.update(doc, ['$set': ['_keywords': tokens]] as com.mongodb.BasicDBObject)
+			}
 		}
+		render "Text indexing of ${params.project} took ${System.currentTimeMillis() - start} ms"
 	}
 }
