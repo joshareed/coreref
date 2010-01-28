ef7a3852c6f6a035933f32b738fd94922b093ad8
Josh Reed <jareed@andrill.org>
2010-01-28 11:33:23 -0600

    Darkened the edges of the gradient a bit
    --HG--
    extra : convert_revision : efafedb6f8cfa8c8be40cb2dcbe1701ba7f11294

--- grails-app/controllers/coreref/ResourcesController.groovy
+++ grails-app/controllers/coreref/ResourcesController.groovy
@@ -14,7 +14,7 @@ class ResourcesController {
 		def height = params.opt as double
 		BufferedImage overlay = new BufferedImage(5, height as int, BufferedImage.TYPE_INT_ARGB)
 		def g2d = overlay.createGraphics()
-		g2d.paint = new GradientPaint(0f, 0f, new Color(0, 0, 0, 200), 0f, (height / 2) as float, new Color(0, 0, 0, 0), true)
+		g2d.paint = new GradientPaint(0f, 0f, new Color(0, 0, 0, 210), 0f, (height / 2) as float, new Color(0, 0, 0, 0), true)
 		g2d.fill(new Rectangle2D.Double(0, 0, 5, height as int))
 		response.contentType = 'image/png'
 		ImageIO.write(overlay, 'png', response.outputStream)
