0c468d945eb855e1774de701a8bba95316344ce1
Josh Reed <jareed@andrill.org>
2010-01-30 07:56:49 -0600

    Cleaned up the number formatting and base/offset calculation
    --HG--
    extra : convert_revision : 07bb16502216d105bf2f1ba1f2a06458d2d076ae

--- grails-app/controllers/coreref/ProjectController.groovy
+++ grails-app/controllers/coreref/ProjectController.groovy
@@ -1,9 +1,14 @@
 package coreref
 
+import java.text.DecimalFormat
+
 class ProjectController {
 	def mongoService
 
-	private getMin(id) { return 3 * Math.round((mongoService[id]?.findByClass('Image')?.top ?: 0) / 3) }
+	private static final DecimalFormat DEC = new DecimalFormat('0.##')
+
+	private getMin(id) { mongoService[id]?.findByClass('Image')?.top ?: 0 }
+	private getParts(depth) { [ 3 * Math.floor(depth / 3), ((depth as double) % 3) as BigDecimal] }
 	private getProject(id) { mongoService['_projects'].findById(id) }
 
 	def index = { redirect(action: overview, params: params) }
@@ -16,16 +21,16 @@ class ProjectController {
 	def viewer = {
 		// handle no depth
 		if (params.depth == null) {
-			redirect(action: viewer, params: [ project: params.project, depth: getMin(params.project) ])
+			def (base, offset) = getParts(getMin(params.project))
+			redirect(url: createLink(controller: 'project', action: 'viewer', params: [project: params.project, depth: DEC.format(base)]) + "#${DEC.format(offset)}")
 			return
 		}
 
 		// check to see if depth is multiple of 3
 		def depth = params.depth as double
 		if (depth % 3 > 0) {
-			def base = 3 * Math.floor(depth / 3)
-			def offset = depth % 3
-			redirect(url: createLink(controller: 'project', action: 'viewer', params: [project: params.project, depth: base]) + "#${offset}")
+			def (base, offset) = getParts(depth)
+			redirect(url: createLink(controller: 'project', action: 'viewer', params: [project: params.project, depth: DEC.format(base)]) + "#${DEC.format(offset)}")
 			return
 		}
 
@@ -58,8 +63,8 @@ class ProjectController {
 				r.top = doc.top as BigDecimal
 				if (doc.top != doc.base) r.base = doc.base as BigDecimal
 				r.title = doc.type ?: doc['class']
-				def depth = 3 * Math.floor(r.top / 3) as BigDecimal
-				r.link = createLink(controller:'project', action: 'viewer', params: [depth: depth, project: project.id]) + "#${(r.top - depth)}"
+				def (base, offset) = getParts(r.top)
+				r.link = createLink(controller:'project', action: 'viewer', params: [depth: DEC.format(base), project: project.id]) + "#${DEC.format(offset)}"
 				r.text = doc.text ?: doc?.code?.replaceAll(',', ' ')
 				r
 			}
