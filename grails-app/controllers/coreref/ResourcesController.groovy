7341cbd453242ca5656ba6e9f4c233f3753bedd1
Josh Reed <jareed@andrill.org>
2010-02-04 14:58:48 -0600

    Add caching headers to the overlay generation
    --HG--
    extra : convert_revision : 412efc05bc5fb61d8524a2ef5b416506e8b6cbb1

--- grails-app/controllers/coreref/ResourcesController.groovy
+++ grails-app/controllers/coreref/ResourcesController.groovy
@@ -10,13 +10,23 @@ import javax.imageio.ImageIO
 class ResourcesController {
 
 	// generate a properly sized overlay image
-    def overlay = {
+	def overlay = {
+		// generate an overlay
 		def height = params.opt as double
 		BufferedImage overlay = new BufferedImage(5, height as int, BufferedImage.TYPE_INT_ARGB)
 		def g2d = overlay.createGraphics()
 		g2d.paint = new GradientPaint(0f, 0f, new Color(0, 0, 0, 210), 0f, (height / 2) as float, new Color(0, 0, 0, 0), true)
 		g2d.fill(new Rectangle2D.Double(0, 0, 5, height as int))
+		g2d.dispose()
+
+		// setup caching headers
 		response.contentType = 'image/png'
+		def nowPlusDay = new Date().time + 86400000
+		response.addHeader("Last-Modified", String.format('%ta, %<te %<tb %<tY %<tH:%<tM:%<tS %<tZ', new Date()))
+		response.addHeader("Expires", String.format('%ta, %<te %<tb %<tY %<tH:%<tM:%<tS %<tZ', new Date(nowPlusDay)))
+		response.addHeader("Cache-Control", "max-age=86400000, must-revalidate")
+
+		// write it out
 		ImageIO.write(overlay, 'png', response.outputStream)
 	}
 }
