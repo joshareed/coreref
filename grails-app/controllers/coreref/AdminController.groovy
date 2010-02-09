0d76cdb5a14184821eab628925a2863468c748d5
Josh Reed <jareed@andrill.org>
2010-02-09 16:28:09 -0600

    Added an endpoint, updateSeries, to calculate the max/min values for each data series via map/reduce. Renamed updateIndex -> updateKeywords.
    --HG--
    extra : convert_revision : aaa4abe20c049421bda6c22d30c162a403585b05

--- grails-app/controllers/coreref/AdminController.groovy
+++ grails-app/controllers/coreref/AdminController.groovy
@@ -3,7 +3,7 @@ package coreref
 class AdminController {
 	def mongoService
 
-	def updateIndex = {
+	def updateKeywords = {
 		long start = System.currentTimeMillis()
 		def collection = mongoService[params.project]
 		collection.find().each { doc ->
@@ -14,9 +14,58 @@ class AdminController {
 				}
 			}
 			if (tokens) {
-				collection.update(doc, ['$set': ['_keywords': tokens]] as com.mongodb.BasicDBObject)
+				collection.update(doc, ['$set': ['_keywords': tokens]])
 			}
 		}
-		render "Text indexing of ${params.project} took ${System.currentTimeMillis() - start} ms"
+		render "Updated keyword index of ${params.project} in ${System.currentTimeMillis() - start} ms"
 	}
+
+	def updateSeries = {
+		long start = System.currentTimeMillis()
+		def collection = mongoService[params.project]
+
+		// use map/reduce to determine the max/min values for each series
+		def mapreduce = collection.mapReduce(SERIES_MAP, SERIES_REDUCE, null, ['class': 'Datum'] as com.mongodb.BasicDBObject)
+		def results = mapreduce.results().inject([:]) { map, doc ->
+			map[doc['_id']] = doc['value']
+			return map
+		}
+
+		// update our config for the project
+		def config = mongoService['_configs']
+		def doc = config.find(['id': params.project])
+		if (doc) {
+			config.update(doc, ['$set': ['data': results]])
+		}
+
+		// clean up
+		mapreduce.drop()
+
+		render "Updated data series index of ${params.project} in ${System.currentTimeMillis() - start}ms"
+	}
+
+	private static final SERIES_MAP = """
+		function() {
+			for (var k in this) {
+				if (k != '_id' && k != 'class' && k != 'type') {
+					emit(k, { max: this[k], min: this[k] });
+				}
+			}
+		}
+"""
+
+	private static final SERIES_REDUCE = """
+		function(key, values) {
+			var result = { max: -9999, min: 9999 };
+			for (var i = 0; i < values.length; i++ ) {
+				if (values[i].max > result.max && values[i].max != '') {
+					result.max = values[i].max;
+				}
+				if (values[i].min < result.min && values[i].min != '') {
+					result.min = values[i].min;
+				}
+			}
+			return result;
+		}
+"""
 }
