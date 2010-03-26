1e48f375391e5a83dbe717ba502901b9a448cbea
Josh Reed <jareed@andrill.org>
2010-03-26 13:40:39 -0500

    Added HTTP basic auth support

--- grails-app/conf/ProjectFilters.groovy
+++ grails-app/conf/ProjectFilters.groovy
@@ -15,11 +15,27 @@ class ProjectFilters {
 				}
 
 				// ensure our project is defined in '_projects'
-				//def project = projects.find(id: params.project)
 				def project = projects.findById(params.project)
 				if (!project) {
 					response.sendError(404, "Invalid project: '${params.project}'")
 					return false
+				}
+				
+				// ensure proper auth if secure project
+				if (project['_secure']) {
+					def auth = request.getHeader('Authorization')
+					if (!auth) {
+						response.addHeader("WWW-Authenticate", "Basic realm=\"" + params.project + "\"")
+						response.sendError(401, "Authorization required")
+						return false
+					}
+					
+					def credentials =  new String(new sun.misc.BASE64Decoder().decodeBuffer(auth - 'Basic ')).split(':')
+					if (credentials[0] != project['_secure'].user || credentials[1] != project['_secure'].pass) {
+						response.addHeader("WWW-Authenticate", "Basic realm=\"" + project.name + "\"")
+						response.sendError(401, "Authorization required")
+						return false
+					}
 				}
 
 				return true
