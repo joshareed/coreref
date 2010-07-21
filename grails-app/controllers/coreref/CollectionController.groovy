cea435a3ebb6da9bc8cbe8efc3bae30dae37cf8a
Josh Reed <jareed@andrill.org>
2010-07-21 11:12:46 -0500

    Handle missing '_collections' collection with a HTTP 500. Handle special collection 'all' and assume id is a program if it doesn't match a declared collection.

--- grails-app/controllers/coreref/CollectionController.groovy
+++ grails-app/controllers/coreref/CollectionController.groovy
@@ -4,20 +4,40 @@ class CollectionController {
 	def mongoService
 
     def index = {
-		def collection = mongoService['_collections'].find('_id': params.collection)
+		def collections = mongoService['_collections']
+		if (!collections) {
+			response.sendError(500, "_collections not initialized")
+			return false
+		}
+		
+		def matching
+		def collection = collections.find('_id': params.collection)
 		if (collection) {
 			def query = [:]
 			transform(collection.query, query)
-			def matching = mongoService['_projects'].findAll(query).sort(program: 1, leg: 1, site: 1, hole: 1).collect { SearchUtils.clean(it) }
-			def projects = [:]
-			matching.groupBy { it.program }.each { program, list ->
-				projects[program] = list.groupBy { it.leg }
-			} 
-			collection.projects = projects
-			return [collection: collection]
+			matching = mongoService['_projects'].findAll(query).sort(program: 1, expedition: 1, site: 1, hole: 1).collect { SearchUtils.clean(it) }
 		} else {
-			response.sendError(404, "Invalid collection: '${params.collection}'")
+			if (params.collection == 'all') {
+				matching = mongoService['_projects'].findAll([:]).sort(program: 1, expedition: 1, site: 1, hole: 1).collect { SearchUtils.clean(it) }
+				collection = [name: 'All Projects', description: 'This collection contains all project available at CoreRef']
+			} else {
+				matching = mongoService['_projects'].findAll(program: params.collection).sort(program: 1, expedition: 1, site: 1, hole: 1).collect { SearchUtils.clean(it) }
+				collection = [name: "${params.project} Expeditions", description: "All ${params.project} Expeditions"]
+			}
 		}
+
+		if (!matching) {
+			response.sendError(404, "Invalid collection: '${params.collection}'")
+			return false
+		}
+
+		// render our projects
+		def projects = [:]
+		matching.groupBy { it.program }.each { program, list ->
+			projects[program] = list.groupBy { it.expedition ?: it.leg }
+		} 
+		collection.projects = projects
+		return [collection: collection]
 	}
 	
 	private def transform(from, to) {
