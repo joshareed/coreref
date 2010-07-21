ab8a16fa18d152f88c0f2a49a98e5e106320ff34
Josh Reed <jareed@andrill.org>
2010-07-21 11:13:08 -0500

    Handle no '_searches' collection with a HTTP 500

--- grails-app/controllers/coreref/RecentController.groovy
+++ grails-app/controllers/coreref/RecentController.groovy
@@ -6,9 +6,15 @@ class RecentController {
 	def mongoService
 
 	def searches = {
+		def searches = mongoService['_searches']
+		if (!searches) {
+			sendError(500, '_searches not initialized')
+			return false
+		}
+
 		def query = [:]
 		if (params?.project) { query.project = params.project }
-		def results = mongoService['_searches'].findAll(query).sort('$natural': -1).limit(10).collect { SearchUtils.clean(it) }.unique { it.query + ":" + it.project }
+		def results = searches.findAll(query).sort('$natural': -1).limit(10).collect { SearchUtils.clean(it) }.unique { it.query + ":" + it.project }
 		if (params.callback) {
 			render(contentType: 'application/json', text: "${params.callback}(${results as JSON})")
 		} else {
